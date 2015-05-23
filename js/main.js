d7b2b86dda366592703d7d862ec2b20b1baeea2c
mbellman <mbellman@nwe.com>
2015-05-23 14:19:34 -0700

    Smarter note placement, preparations for other mouse actions and tools

--- js/main.js
+++ js/main.js
@@ -3,8 +3,11 @@
 var init = false;
 
 var page = {
-    width  : $(window).width(),
-    height : $(window).height()
+    $window : $(window),
+    width   : $(window).width(),
+    height  : $(window).height(),
+    $body   : $('body'),
+    $music  : $('.music')
 }
 
 var instruments = {
@@ -20,8 +23,8 @@ var selectedInstrument = 1;
 // ---------------------------------------
 // -------------- Handlers ---------------
 function handleResize() {
-    page.width = $(window).width();
-    page.height = $(window).height();
+    page.width  = page.$window.width();
+    page.height = page.$window.height();
 
     if(init) {
         updateCorners();
@@ -68,16 +71,16 @@ var tools = {
         },
 
         'br' : null,
-        
+
         'Play' : function() {
         },
-        
+
         'Pause' : function() {
         },
-        
+
         'Restart' : function() {
         },
-        
+
         'br2' : null,
 
         'Demos' : function() {
@@ -99,7 +102,7 @@ var tools = {
             selectedTool = 3;
             toolbar.hideOtherDrops();
         },
-        
+
         'Select' : function() {
             selectedTool = 4;
             toolbar.hideOtherDrops();
@@ -237,20 +240,43 @@ function generateUI() {
 
 // ---------------------------------------
 // -------------- Music roll -------------
+function getTile(mouseX, mouseY) {
+    var container = $('.music').offset();
 
-function putNote(x, y) {
+    return {
+        x: Math.floor( (mouseX - container.left) / 30 ),
+        y: Math.floor( (mouseY - container.top) / 30 )
+    }
+}
+
+function putPreviewNote(x, y) {
+    var pNote = $('<div/>', {
+        class : 'preview-note',
+        css   : {
+            width : '0px',
+            top   : (y*30)+8 + 'px',
+            left  : x*30 + 'px',
+        }
+    });
+    
+    page.$music.append(pNote);
+
+    return pNote;
+}
+
+function putNote(x, y, width) {
     var note = $('<div/>', {
         class : 'note',
-        css : {
-            width : '30px',
+        css   : {
+            width : width + 'px',
             top   : (y*30)+5 + 'px',
             left  : x*30 + 'px'
         }
     });
-    
+
     note.scale(0).css('opacity', '0');
-    $('.music').append( note.addClass(instruments[selectedInstrument]) );
-    
+    page.$music.append( note.addClass(instruments[selectedInstrument]) );
+
     note.delay(25).queue(function(){
         $(this).scale(1).css('opacity', '1');
         $(this).dequeue();
@@ -264,6 +290,13 @@ function eraseNote(x, y) {
 
 // ---------------------------------------
 // -------------- DOM/Events -------------
+var mouseDrag = false;
+var dragTimer = null;
+
+function resetMouseDrag() {
+    mouseDrag = false;
+}
+
 $(document).ready(function(){
 
     generateUI();
@@ -290,40 +323,105 @@ $(document).ready(function(){
             toolbar.hideOtherDrops();
             activeDrop = -1;
             $('.nav').removeClass('active');
-            
+
             setTimeout(function(){
                 toolbarActive = false;
             }, 25);
         }
     });
-    
-    // Music roll actions (placing/interacting with notes, drag-scrolling)
+
+    // Placing or erasing a single note
     $('.music').click(function(e){
-        if(!toolbarActive) {
+        if(!toolbarActive && !mouseDrag) {
             var mouseX = e.clientX;
             var mouseY = e.clientY;
-            
-            var container = $('.music').offset();
-            
-            var tileX = Math.floor( (mouseX - container.left) / 30 );
-            var tileY = Math.floor( (mouseY - container.top) / 30 );
-            
+
+            var tile = getTile(mouseX, mouseY);
+
             switch(selectedTool) {
                 case 1:     // Placing a new note
-                    putNote(tileX, tileY);
+                    putNote(tile.x, tile.y, 30);
                     break;
                 case 2:     // Erasing a note
-                    eraseNote(tileX, tileY);
-                    break;
-                case 3:     // Dragging the music roll
+                    eraseNote(tile.x, tile.y);
                     break;
             }
         }
     });
 
+    // Dragging along the music roll
+    $('.music').on('mousedown', function(e){
+        var mouseX = e.clientX;
+        var mouseY = e.clientY;
+
+        var tile = getTile(mouseX, mouseY);
+
+        var delta = {x: 0, y: 0};
+
+        // For the first mousedown action, we're only concerned
+        // about placing and erasing notes - selection/scroll
+        // update continuously as the mouse is moved
+        switch(selectedTool) {
+            case 1:     // Placing a new note
+                var pNote = putPreviewNote(tile.x, tile.y);
+            case 2:     // Erasing notes
+        }
+
+        page.$body.on('mousemove', function(e){
+            mouseDrag = true;
+            clearTimeout(dragTimer);
+            
+            var nMouseX = e.clientX;
+            var nMouseY = e.clientY;
+
+            delta.x = nMouseX - mouseX;
+            delta.y = nMouseY - mouseY;
+
+            var newTile = getTile(nMouseX, nMouseY);
+
+            switch(selectedTool) {
+                case 1:     // Placing an elongated new note
+                    pNote.css('width', 30+(30*(newTile.x - tile.x)) + 'px');
+                    break;
+                case 2:     // Erasing any notes covered by the mouse
+                    break;
+                case 3:     // Selecting notes
+                    break;
+                case 4:     // Dragging the music roll view
+                    break;
+            }
+        });
+
+        page.$body.on('mouseup', function(e){
+            page.$body.off('mousemove mouseup');
+            
+            var fMouseX = e.clientX;
+            var fMouseY = e.clientY;
+            
+            var finalTile = getTile(fMouseX, fMouseY);
+
+            // Only note placement and note selection should trigger
+            // an action after the mouse is released
+            switch(selectedTool) {
+                case 1:     // Placing an elongated new note
+                    pNote.remove();
+                    putNote(tile.x, tile.y, 30+(30*(finalTile.x - tile.x)));
+                    break;
+                case 3:     // Selecting notes (group all selected)
+                    break;
+            }
+            
+            dragTimer = setTimeout(resetMouseDrag, 250);
+        });
+    });
+
     init = true;
     
-    $(window).resize(handleResize).resize();
+    page.$body.on('mousedown', function(e){
+        e.preventDefault();
+    });
+
+    page.$window.resize(handleResize).resize();
 });
 
 $.fn.scale = function(ratio) {
