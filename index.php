5041fb380fa3cf931cd56fd7a77448c7fb18c7bc
Malcolm Bellman <mbellman@nwe.com>
2015-05-22 18:14:48 -0700

    stylesheet name edit

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <title>Page Title</title>
         <style type="text/css">
-            @import url('css/style.css');
+            @import url('css/styles.css');
         </style>
 
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
